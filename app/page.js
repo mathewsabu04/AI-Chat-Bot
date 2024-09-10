9167dd6bbb5446efe15cd5b489884d5ca56115f1
Mathew Sabu <internshipmathewsabu04@gmail.com>
2024-09-10 14:18:06 -0400

    Changed backgroiund and added a title

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
-'use client'
+"use client";
 import { useState } from "react";
 import Image from "next/image";
+import { Box } from "@mui/material";
 
 export default function Home() {
-
+  return (
+    <Box textAlign="center">
+      <h1 className="welcome-title">Welcome to MatBot</h1>
+    </Box>
+  );
 }
+
+// Add this CSS to your styles
